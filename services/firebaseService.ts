ec7e090d8253b39e3411d2703fc64e47349e0bc9
gabboggie <gmrivera.e@gmail.com>
2026-01-28 15:24:17 -0600

    v2.3.4

---
+++ services/firebaseService.ts
@@ -0,0 +1,18 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBtKIzoCSXZGTxC-gRxIArbnVuMmqG2MEI",
+  authDomain: "juntas-bitacora.firebaseapp.com",
+  projectId: "juntas-bitacora",
+  storageBucket: "juntas-bitacora.firebasestorage.app",
+  messagingSenderId: "1054570412142",
+  appId: "1:1054570412142:web:3312e3c448b4f40eb0ce7f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
